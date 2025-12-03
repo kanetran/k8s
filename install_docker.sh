df7f1e31df15ac352a8952c6eb4477427237f170
Anh Tran <tuananhbk1996@gmail.com>
2025-12-03 09:33:59 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,5 +3,5 @@
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 docker --version
